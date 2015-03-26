ee6442e1546d8b5688d5808d1184a3af1f94bc75
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-03-25 21:58:51 -0500

    Arreglado headers

--- api.py
+++ api.py
@@ -27,7 +27,7 @@ class IndexHandler(web.RequestHandler):
         self.render("index.html")
 
 class ApiBaseHandler(web.RequestHandler):
-    def set_headers(self):
+    def set_default_headers(self):
         self.set_header("Content-Type", "application/json; charset=utf-8")
 
     def on_connection_close(self):
